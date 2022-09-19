4e8438c18b9ad80ac4d71093ce3bd6ffbd235f3f
miuractech <miuractech@gmail.com>
2022-09-19 10:57:33 +0530

    init

---
+++ apps/admin/src/app/app.tsx
@@ -0,0 +1,13 @@
+import './app.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="admin" />
+      <div />
+    </>
+  );
+}
+
+export default App;
